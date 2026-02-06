4c3f5b778405108257fd06297835614e661026a5
Alexander gfoxd <sashanara700@gmail.com>
2026-02-06 08:09:30 +0800

    first commit

---
+++ src/main/java/boot/spring/eventmanager_1/EventManager1Application.java
@@ -0,0 +1,13 @@
+package boot.spring.eventmanager_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManager1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManager1Application.class, args);
+    }
+
+}
---
+++ src/test/java/boot/spring/eventmanager_1/EventManager1ApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.spring.eventmanager_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManager1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
